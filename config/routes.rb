8d30077a4acea8b915ff4deedb9350d0ef78491b
jcg2201 <jcg2201@columbia.edu>
2021-03-15 01:59:42 -0400

    profile page set to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#profile'
   devise_for :users
+  resources :users do
+    get 'profile'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
